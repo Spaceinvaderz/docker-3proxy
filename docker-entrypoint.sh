b03a31dd63eda078dad1f73b1f5f1bc543a2b1f2
ErgoZ Riftbit Vaper <ergozru@gmail.com>
2017-11-27 18:11:46 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-set -e
 
-if [ -z "$1" ]; then 
-    PROXY_USR_PWD=$(pwgen -s -N 1)
-else
-    PROXY_USR_PWD=$1
-fi
-
-if [ -z "$2" ]; then 
-    PROXY_ADM_PWD=$(pwgen -s -N 1)
-else
-    PROXY_ADM_PWD=$2
-fi
+if [ -z $PROXY_USR_LOGIN ]; then echo "PROXY_USR_LOGIN is unset";
+if [ -z $PROXY_USR_PWD ]; then echo "PROXY_USR_PWD is unset";
+if [ -z $PROXY_ADMIN_LOGIN ]; then echo "PROXY_ADMIN_LOGIN is unset";
+if [ -z $PROXY_ADMIN_PWD ]; then echo "PROXY_ADMIN_PWD is unset";
 
 echo "
 nserver 8.8.8.8
@@ -19,14 +11,14 @@ nserver 8.8.4.4
 nscache 65536
 nsrecord www.porno.com 127.0.0.1
 daemon
-pidfile /var/run/3proxy.pid
-log /var/log/3proxy.log D
+pidfile /tmp/3proxy.pid
+log /etc/3proxy/3proxy.log D
 rotate 7
-users proxy:CL:$PROXY_USR_PWD
+users $PROXY_USR_LOGIN:CL:$PROXY_USR_PWD
 auth strong
 socks -p3128
 flush
-users admin:CL:$PROXY_ADM_PWD
+users $PROXY_ADMIN_LOGIN:CL:$PROXY_ADMIN_PWD
 maxconn 1
 auth strong
 admin -p31331
@@ -39,8 +31,8 @@ exec /etc/3proxy/3proxy /etc/3proxy/3proxy.cfg
 PROXY_PID=$(cat /var/run/3proxy.pid)
 
 echo "IP address:               $(hostname -I)"
-echo "Proxy user login:         proxy"
+echo "Proxy user login:         $PROXY_USR_LOGIN"
 echo "Proxy user password:      $PROXY_USR_PWD"
-echo "Proxy admin user:         admin"
-echo "Proxy admin password:     $PROXY_ADM_PWD"
+echo "Proxy admin user:         $PROXY_ADMIN_LOGIN"
+echo "Proxy admin password:     $PROXY_ADMIN_PWD"
 echo "Proxy process pid:        $PROXY_PID"
